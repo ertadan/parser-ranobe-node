f7c7fa9cc71c29c2949d3ae7a2b6ade78a9492d6
ertadan <vin021110@gmail.com>
2024-10-21 00:33:46 +0300

    Added few retries to login to avoid network issues. Added function to save chapter. Added TODOs

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ const winston = require('winston');
 const puppeteer = require('puppeteer');
 
 const SAVED_CHAPTERS = path.join(__dirname, 'chapters.json');
+const POPUP_SELECTORS = {
+    indexPage: {
+        header: 'body > div.popup-root > div:nth-child(2) > div.popup__inner > div > div.popup-header > div',
+        checkbox: 'body > div.popup-root > div:nth-child(2) > div.popup__inner > div > div.popup-body > div.form-group._offset > label > input',
+        button: 'body > div.popup-root > div:nth-child(2) > div.popup__inner > div > div.popup-body > div.flex.btns._stretch > button.btn.is-filled.variant-primary.size-lg'
+    },
+    readerPage: {
+        header: 'body > div.popup-root > div > div.popup__inner > div > div.popup-header > div',
+        checkbox: 'body > div.popup-root > div > div.popup__inner > div > div.popup-body > div:nth-child(2) > label > input',
+        button: 'body > div.popup-root > div > div.popup__inner > div > div.popup-body > div.btns._stretch > button.btn.variant-danger'
+    }
+};
 
 // Настройка логировщика
 const logger = winston.createLogger({
@@ -35,46 +47,64 @@ async function readConfig() {
 }
 
 async function login(page, username, password) {
-    logger.info('Вход в систему...');
-    
-    await page.goto('https://v2.slashlib.me/ru/front/auth', { waitUntil: 'networkidle2' });
-    
-    await page.waitForSelector('input[name="login"]', { visible: true });
-    await page.type('input[name="login"]', username);
-    
-    await page.waitForSelector('input[name="password"]', { visible: true });
-    await page.type('input[name="password"]', password);
-    
-    await Promise.all([
-        page.click('button.btn.btn_variant-primary.btn_filled.btn_block.btn_size-lg[type="submit"]'),
-        page.waitForNavigation({ waitUntil: 'networkidle2', timeout: 60000 }),
-    ]);
+    const retries = 3;
+    const delay = 2000;
+    for (let attempt = 1; attempt <= retries; attempt++) {
+        try {
+        logger.info('Вход в систему...');
+        
+        await page.goto('https://v2.slashlib.me/ru/front/auth', { waitUntil: 'networkidle2', timeout: 60000  });
+        
+        await page.waitForSelector('input[name="login"]', { visible: true });
+        await page.type('input[name="login"]', username);
+        
+        await page.waitForSelector('input[name="password"]', { visible: true });
+        await page.type('input[name="password"]', password);
+        
+        await Promise.all([
+            page.click('button.btn.btn_variant-primary.btn_filled.btn_block.btn_size-lg[type="submit"]'),
+            page.waitForNavigation({ waitUntil: 'networkidle2', timeout: 60000 }),
+        ]);
 
-    await page.waitForSelector('button.btn:nth-child(5)', { visible: true });
-    await page.click('button.btn:nth-child(5)');
+        await page.waitForSelector('button.btn:nth-child(5)', { visible: true });
+        await page.click('button.btn:nth-child(5)');
 
-    await page.waitForNavigation({ waitUntil: 'networkidle2', timeout: 60000 });
-    logger.info('Успешно авторизован!');
+        await page.waitForNavigation({ waitUntil: 'networkidle2', timeout: 60000 });
+        logger.info('Ожидаем окончания загрузки страницы...');
+        await page.waitForNavigation({ waitUntil: 'networkidle0', timeout: 30000 });
+        logger.info('Успешно авторизован!');
+
+        return; 
+    } catch (error) {
+        if (error.message.includes('net::ERR_CONNECTION_RESET') && attempt < retries) {
+            logger.warn(`Попытка ${attempt} не удалась: ${error.message}. Повтор через ${delay / 1000} секунд...`);
+            await new Promise(resolve => setTimeout(resolve, delay)); // Задержка перед следующей попыткой
+        } else {
+            logger.error('Ошибка при входе: ' + error.message);
+            throw error; // Пробрасываем ошибку, если достигли лимита попыток или другая ошибка
+            }
+        }
+    }
 }
 
-async function handlePopup(page) {
+async function handlePopup(page, source) {
     logger.info('Обработка всплывающего окна...');
     try {
-        await page.waitForSelector('body > div.popup-root > div:nth-child(2) > div.popup__inner > div > div.popup-header > div', { visible: true, timeout: 15000 });
+        await page.waitForSelector(source.header, { visible: true, timeout: 30000 });
 
-        const headerText = await page.evaluate(() => {
-            const header = document.querySelector('body > div.popup-root > div:nth-child(2) > div.popup__inner > div > div.popup-header > div');
+        const headerText = await page.evaluate((headerSelector) => {
+            const header = document.querySelector(headerSelector);
             return header ? header.textContent : null;
-        });
+        }, source.header);
 
-        if (headerText === 'Внимание') {
+        if (headerText === 'Внимание' || headerText === 'Предупреждение') {
             logger.info('Найдено всплывающее окно с заголовком: ' + headerText);
 
-            await page.click('body > div.popup-root > div:nth-child(2) > div.popup__inner > div > div.popup-body > div.form-group._offset > label > input');
+            await page.click(source.checkbox);
             logger.info("Чекбокс о контенте для взрослых установлен");
 
-            await page.click('body > div.popup-root > div:nth-child(2) > div.popup__inner > div > div.popup-body > div.flex.btns._stretch > button.btn.is-filled.variant-primary.size-lg');
-            logger.info('Нажата кнопка "Мне есть 18+"');
+            await page.click(source.button);
+            logger.info('Кнопка нажата');
 
             logger.info('Всплывающее окно обработано!');
         } else {
@@ -90,7 +120,7 @@ async function getChapters(page, mangaLink) {
     const chaptersUrl = `${mangaLink}?section=chapters`;
     await page.goto(chaptersUrl, { waitUntil: 'networkidle2', timeout: 60000 });
 
-    await handlePopup(page); // Обработка всплывающего окна
+    await handlePopup(page,POPUP_SELECTORS.indexPage); // Обработка всплывающего окна
 
     const chapters = {};
     const scrollPauseTime = 2000; // Увеличьте время ожидания после прокрутки
@@ -147,6 +177,63 @@ async function getChapters(page, mangaLink) {
     return chaptersArray; // Возвращаем массив глав
 }
 
+async function saveChapters(page, chapters) {
+    for (const chapter of chapters) {
+        logger.info('Сохраняем главу... ' + chapter.title);
+        await page.goto(chapter.link, { waitUntil: 'networkidle2' });
+
+        // Обработка всплывающего окна (если оно есть)
+        await handlePopup(page, POPUP_SELECTORS.readerPage);
+
+        // Создание папки для сохранения изображений
+        // TODO: save all files into downloads/ folder
+        // TODO: chapter.title must be trimmed from spaces!
+        const chapterDir = path.join(__dirname, chapter.title);
+        if (!fs.existsSync(chapterDir)) {
+            fs.mkdirSync(chapterDir);
+            logger.info(`Создана папка для главы: ${chapterDir}`);
+        }
+
+        // Получение URL изображения
+        const imageUrl = await page.evaluate(() => {
+            const imgElement = document.querySelector('.yj_kw.yj_kx');
+            return imgElement ? imgElement.src : null;
+        });
+
+        if (!imageUrl) {
+            logger.info(`Не удалось найти изображение для главы "${chapter.title}".`);
+            continue; // Переходим к следующей главе, если изображения нет
+        }
+
+        logger.info(`Найдено изображение для главы "${chapter.title}": ${imageUrl}`);
+
+        // Загрузка изображения
+        try {
+            // TODO: work with pagination because we save only the first image in chapter
+            const viewSource = await page.goto(imageUrl, { waitUntil: 'networkidle2' });
+
+            const imagePath = path.join(chapterDir, 'image.jpg'); // Сохраняем с именем image.png
+            fs.writeFileSync(imagePath, await viewSource.buffer()); // Сохраняем изображение
+
+            logger.info(`Изображение сохранено: ${imagePath}`);
+        } catch (error) {
+            logger.error(`Ошибка при загрузке изображения для главы "${chapter.title}": ${error.message}`);
+            continue; // Переходим к следующей главе в случае ошибки
+        }
+
+        // Клик по изображению для перехода к следующей части
+        // TODO: change this logic because we move backwards!
+        await page.evaluate(() => {
+            const imgElement = document.querySelector('.yj_kw.yj_kx');
+            if (imgElement) {
+                imgElement.click();
+            }
+        });
+
+    }
+}
+
+
 async function main() {
     const { username, password, mangaLink } = await readConfig();
     const browser = await puppeteer.launch({ headless: false });
@@ -163,10 +250,10 @@ async function main() {
         logger.info('Файл chapters.json не найден, извлекаем главы...');
         chapters = await getChapters(page, mangaLink);
     }
-    
+    await saveChapters(page, chapters);
 
     // Логика для работы с главами (например, вывод в лог или другое)
-    logger.info('Готовые главы: ' + JSON.stringify(chapters));
+    //logger.info('Готовые главы: ' + JSON.stringify(chapters));
 
     await browser.close();
 }
